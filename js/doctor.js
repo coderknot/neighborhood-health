bc700ed2f9aef6a0e2711257ec125ce5ba78e070
Chris Carr <coder.knot@gmail.com>
2017-04-21 17:22:56 -0700

    update api call to use callback function

--- js/doctor.js
+++ js/doctor.js
@@ -1,18 +1,33 @@
 var apiKey = require('./../.env').apiKey;
 
 function Doctor() {
-
 }
 
+Doctor.prototype.getDoctors = function(symptom, displayDoctors) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' +
+    symptom +
+    '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
+    .then(function(response) {
+      var doctors = [];
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
-   .then(function(result) {
-      console.log(result);
-    })
-   .fail(function(error){
-      console.log("fail");
-    });
+      response.data.forEach(function(doctor, displayDoctors) {
+        var singleDoctor = {};
+        singleDoctor.firstName = doctor.profile.first_name;
+        singleDoctor.lastName = doctor.profile.last_name;
+        singleDoctor.title = doctor.profile.title;
+        singleDoctor.bio = doctor.profile.bio;
+        if (doctor.profile.image_url === null) {
+          singleDoctor.image = "./../img/default.jpg";
+        } else {
+          singleDoctor.image = doctor.profile.image_url;
+        }
+        doctors.push(singleDoctor);
+      });
+
+      displayDoctors(doctors);
+    }).fail(function(error){
+        console.log(error);
+  });
 };
 
 exports.doctorModule = Doctor;
