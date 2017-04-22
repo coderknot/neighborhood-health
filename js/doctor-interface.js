14e03eb09d7f282280c3b2c326a56f7b69e94036
Chris Carr <coder.knot@gmail.com>
2017-04-21 17:22:27 -0700

    add callback funtion from api call to display doctors

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,12 +1,23 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctors = function (doctors) {
+
+  doctors.forEach(function(doctor){
+    $("#doctor-search-results").append("<h3>" + doctor.firstName +
+      " " + doctor.lastName + ", " + doctor.title + "</h3");
+    $("#doctor-search-results").append("<img src='" + doctor.image + "' + alt='Doctor Image'>");
+    $("#doctor-search-results").append("<p>" + doctor.bio + "</p>");
+    $("#doctor-search-results").append("<br>");
+  });
+};
+
 $(document).ready(function() {
   var currentDoctorObject = new Doctor();
 
   $("#doctor-search").click(function() {
-    var medicalIssue = $("#medical-issue").val();
-    console.log(medicalIssue);
-    currentDoctorObject.getDoctors(medicalIssue);
-    // $("medical-issue").val("");
+    $("#doctor-search-results").empty();
+    var symptom = $("#symptom").val();
+    currentDoctorObject.getDoctors(symptom, displayDoctors);
+    $("#symptom").val("");
   });
 });
